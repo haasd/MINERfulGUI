4772db399c64693820706a772305015033258be9
Dominik Haas <HaasDominik@outlook.com>
2019-08-17 23:47:44 +0200

    add trace and event information to area event-log generator

---
+++ src/minerful/gui/common/TraceInfo.java
@@ -0,0 +1,28 @@
+package minerful.gui.common;
+
+import org.deckfour.xes.model.XTrace;
+
+public class TraceInfo {
+	private String label;
+	private XTrace trace;
+	
+	public TraceInfo(String label, XTrace trace) {
+		this.label = label;
+		this.trace = trace;
+	}
+	
+	public String getLabel() {
+		return label;
+	}
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	public XTrace getTrace() {
+		return trace;
+	}
+	public void setTrace(XTrace trace) {
+		this.trace = trace;
+	}
+	
+	
+}
--- src/minerful/gui/controller/EventLogGeneratorTabController.java
+++ src/minerful/gui/controller/EventLogGeneratorTabController.java
@@ -3,30 +3,51 @@ package minerful.gui.controller;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.ResourceBundle;
 import java.util.concurrent.ExecutionException;
 
+import org.deckfour.xes.extension.std.XExtendedEvent;
+import org.deckfour.xes.model.XEvent;
 import org.deckfour.xes.model.XLog;
 import org.deckfour.xes.model.XTrace;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.control.Pagination;
-import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.util.Callback;
+import minerful.gui.common.EventInfo;
 import minerful.gui.common.MinerfulGuiUtil;
 import minerful.gui.common.ModelInfo;
 import minerful.gui.common.ProgressForm;
+import minerful.gui.common.TraceInfo;
 import minerful.gui.common.ValidationEngine;
 import minerful.logmaker.MinerFulLogMaker;
 import minerful.logmaker.params.LogMakerParameters;
 
 public class EventLogGeneratorTabController extends AbstractController implements Initializable {
+	
+	private final ObservableList<TraceInfo> eventLogList =
+	        FXCollections.observableArrayList();
+	
+	private final ObservableList<EventInfo> traceInfoList =
+	        FXCollections.observableArrayList();
 	
 	@FXML
 	TextField minEventsPerTrace;
@@ -41,15 +62,65 @@ public class EventLogGeneratorTabController extends AbstractController implement
 	Pagination pagination;
 	
 	@FXML
-	TableView<String> traceLog;
+	ListView<TraceInfo> eventLog;
+	
+	@FXML
+	ListView<EventInfo> traceInfo;
 	
 	private ModelInfo modelInfo;
+	
+	private Integer traceNumber = 0;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		minEventsPerTrace.setTextFormatter(ValidationEngine.getNumericFilter());
 		maxEventsPerTrace.setTextFormatter(ValidationEngine.getNumericFilter());
 		tracesInLog.setTextFormatter(ValidationEngine.getNumericFilter());
+		
+		eventLog.setCellFactory(param -> {
+				ListCell<TraceInfo> listCell = new ListCell<>() {
+			    @Override
+			    protected void updateItem(TraceInfo item, boolean empty) {
+			        super.updateItem(item, empty);
+	
+			        if (empty || item == null || item.getLabel() == null) {
+			            setText(null);
+			        } else {
+			            setText(item.getLabel());
+			        }
+			    }
+			};
+			
+			listCell.setOnMouseClicked(event -> {
+		        if (! listCell.isEmpty() && event.getButton()==MouseButton.PRIMARY 
+			             && event.getClickCount() == 2) {
+		        	
+		        		Iterator<XEvent> it = eventLogList.get(listCell.getIndex()).getTrace().iterator();
+		        		traceInfoList.clear();
+		        		int i = 0;
+		        		
+		        		while(it.hasNext()) {
+		        			i++;
+		        			XExtendedEvent extxevent = new XExtendedEvent(it.next());
+
+		        			traceInfoList.add(new EventInfo(i,extxevent.getName(), extxevent));
+		        		}
+			        	
+			        }
+			});
+			return listCell;
+		});
+		
+		eventLog.setItems(eventLogList);
+		
+		traceInfo.setCellFactory(new Callback<ListView<EventInfo>, ListCell<EventInfo>>() {
+            @Override
+            public ListCell<EventInfo> call(ListView<EventInfo> listView) {
+                return new EventListCell();
+            }
+	    });
+		
+		traceInfo.setItems(traceInfoList);
 	}
 	
 	@FXML
@@ -99,9 +170,12 @@ public class EventLogGeneratorTabController extends AbstractController implement
 				new Thread(createLog).start();
 				XLog log = createLog.get();
 				Iterator<XTrace> it = log.iterator();
+				Integer number=0;
 				while(it.hasNext()) {
+					number++;
 					XTrace xtrace = it.next();
-				}
+					eventLogList.add(new TraceInfo("Trace " + number, xtrace));
+				}	
 				
 				logMak.storeLog();
 			} catch (IOException e) {
@@ -117,6 +191,48 @@ public class EventLogGeneratorTabController extends AbstractController implement
 		}
 		
 	}
+	
+	private class EventListCell extends ListCell<EventInfo> {
+        private HBox content;
+        private Label activityName = new Label();
+        private Label timestamp = new Label();
+        private Label role = new Label();
+        private Label transition = new Label();
+        private Label resource = new Label();
+        private Label index = new Label();
+
+        public EventListCell() {
+            super();
+
+            VBox vBox = new VBox(activityName,timestamp,role,transition,resource);
+            content = new HBox(index, vBox);
+            content.setSpacing(10);
+            content.setAlignment(Pos.CENTER_LEFT);
+            content.getStyleClass().add("trace-info");
+            activityName.getStyleClass().add("trace-info-activity-label");
+            timestamp.getStyleClass().add("trace-info-content");
+            role.getStyleClass().add("trace-info-content");
+            transition.getStyleClass().add("trace-info-content");
+            resource.getStyleClass().add("trace-info-content");
+            index.getStyleClass().add("trace-info-index");
+        }
+
+        @Override
+        protected void updateItem(EventInfo item, boolean empty) {
+            super.updateItem(item, empty);
+            if (item != null && !empty) { // <== test for null item and empty parameter
+            	activityName.setText(item.getLabel());
+            	timestamp.setText(item.getEvent().getTimestamp().toString());
+            	role.setText(item.getEvent().getRole());
+            	transition.setText(item.getEvent().getTransition());
+            	resource.setText(item.getEvent().getResource());
+            	index.setText(item.getIndex().toString());
+                setGraphic(content);
+            } else {
+                setGraphic(null);
+            }
+        }
+    }
 	
 	public ModelInfo getModelInfo() {
 		return modelInfo;
