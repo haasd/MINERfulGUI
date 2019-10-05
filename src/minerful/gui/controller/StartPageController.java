5df7068a66501ebe031b0f33c4fd1386e234fff6
Dominik Haas <HaasDominik@outlook.com>
2019-10-05 19:05:22 +0200

    Highlight active menu-point

--- src/minerful/gui/controller/StartPageController.java
+++ src/minerful/gui/controller/StartPageController.java
@@ -2,7 +2,9 @@ package minerful.gui.controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
 
@@ -15,6 +17,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
@@ -46,6 +49,24 @@ public class StartPageController extends AbstractController implements Initializ
     private ImageView imageView;
 	
 	@FXML
+    private Button discoverButton;
+	
+	@FXML
+    private Button generateButton;
+	
+	@FXML
+    private Button simplifyButton;
+	
+	@FXML
+    private Button eventLogButton;
+	
+	@FXML
+    private Button automataButton;
+	
+	@FXML
+    private Button fitnessButton;
+	
+	@FXML
 	private FlowPane helpIconPane;
 	
 	@FXML
@@ -71,6 +92,8 @@ public class StartPageController extends AbstractController implements Initializ
 	
 	private Map<String,GridPane> gridPanes = new HashMap<>();
 	
+	private List<Button> buttons = new ArrayList<>();
+	
 	@Override
     public void initialize(URL location, ResourceBundle resources) {
 		initDocumentation();
@@ -79,6 +102,13 @@ public class StartPageController extends AbstractController implements Initializ
 		infoSimplify.setOnMouseClicked(e -> openDocumentation(GuiConstants.SIMPLIFY));
 		infoGenerateAutomata.setOnMouseClicked(e -> openDocumentation(GuiConstants.GENERATE_AUTOMATA));
 		infoPerformCheck.setOnMouseClicked(e -> openDocumentation(GuiConstants.PERFORM_CHECK));
+		
+		buttons.add(discoverButton);
+		buttons.add(eventLogButton);
+		buttons.add(generateButton);
+		buttons.add(simplifyButton);
+		buttons.add(fitnessButton);
+		buttons.add(automataButton);
     }
 	
 	private void initDocumentation() {
@@ -109,6 +139,7 @@ public class StartPageController extends AbstractController implements Initializ
         	handlePane("discover", "pages/discover/Discover.fxml");
  
         	currentView = "discover";
+        	highlightCurrentMenu(event);
     	} 
     }
     
@@ -124,6 +155,8 @@ public class StartPageController extends AbstractController implements Initializ
     		if(!newWindow) {
     			rootPane.getChildren().set(0, gridPane);
     		} 
+    		
+    		
 			
 			return gridPane;
 			
@@ -147,57 +180,62 @@ public class StartPageController extends AbstractController implements Initializ
     }
     
     @FXML
-    private void openModelGenerator() {
+    private void openModelGenerator(ActionEvent event) {
     	if(currentView != "modelgenerator") {
     		logger.info("Open Model Generator");
     		
     		handlePane("modelgenerator", "pages/modelgenerator/ModelGenerator.fxml");
     		
         	currentView = "modelgenerator";
+        	highlightCurrentMenu(event);
     	}
     }
     
     @FXML
-    private void openEventLogGenerator() {
+    private void openEventLogGenerator(ActionEvent event) {
     	if(currentView != "eventloggenerator") {
     		logger.info("Open EventLog Generator");
     		
     		handlePane("eventloggenerator", "pages/eventloggenerator/EventLogGenerator.fxml");
     		
         	currentView = "eventloggenerator";
+        	highlightCurrentMenu(event);
     	}
     }
     
     @FXML
-    private void openSimplifier() {
+    private void openSimplifier(ActionEvent event) {
     	if(currentView != "simplifier") {
         	logger.info("Open Simplification");
         	
         	handlePane("simplifier", "pages/simplifier/Simplifier.fxml");
         	
         	currentView = "simplifier";
+        	highlightCurrentMenu(event);
     	}
     }
     
     @FXML
-    private void openAutomataGenerator() {
+    private void openAutomataGenerator(ActionEvent event) {
     	if(currentView != "automatagenerator") {
         	logger.info("Open Automata Generator");
         	
         	handlePane("automatagenerator", "pages/automatagenerator/AutomataGenerator.fxml");
 
         	currentView = "automatagenerator";
+        	highlightCurrentMenu(event);
     	}
     }
     
     @FXML
-    private void openFitnessChecker() {
+    private void openFitnessChecker(ActionEvent event) {
     	if(currentView != "fitnesschecker") {
         	logger.info("Open Fitness-Check");
         	
         	handlePane("fitnesschecker", "pages/fitnesschecker/FitnessChecker.fxml");
 
         	currentView = "fitnesschecker";
+        	highlightCurrentMenu(event);
     	}
     }
     
@@ -259,6 +297,17 @@ public class StartPageController extends AbstractController implements Initializ
 			rootPane.getChildren().set(0, gridPanes.get(paneName));
 		}
     
+    }
+    
+    private void highlightCurrentMenu(ActionEvent event) {
+    	for(Button btn : buttons) {
+    		if(event.getSource() == btn) {
+    			btn.getStyleClass().add("active-menu");
+    		} else {
+    			btn.getStyleClass().remove("active-menu");
+    		}
+    	}
+    	
     }
     
 }
