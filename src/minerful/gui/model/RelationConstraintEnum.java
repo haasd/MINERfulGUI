5c8fc9cdccfac5ce572f663ff38dd70e8846eda6
Dominik Haas <HaasDominik@outlook.com>
2020-04-07 22:28:23 +0200

    Export template names capitalized

--- src/minerful/gui/model/RelationConstraintEnum.java
+++ src/minerful/gui/model/RelationConstraintEnum.java
@@ -1,20 +1,20 @@
 package minerful.gui.model;
 
 public enum RelationConstraintEnum {
-	RESPONDED_EXISTENCE("RespondedExistence",true, new Template("respondedExistence", true, true, false, false, false, false, false, false) ),
-	RESPONSE("Response", true, new Template("response", false, true, false, false, false, false, false, false)),
-	ALTERNATE_RESPONSE("AlternateResponse", true, new Template("alternateResponse", false, true, false, false, true, false, false, false)),
-	CHAIN_RESPONSE("ChainResponse", true, new Template("chainResponse", false, true, false, false, false, false, true, false)),
-	PRECEDENCE("Precedence", true, new Template("precedence", false, false, true, false, false, false, false, false)),
-	ALTERNATE_PRECEDENCE("AlternatePrecedence", true, new Template("alternatePrecedence", false, false, true, false, false, true, false, false)),
-	CHAIN_PRECEDENCE("ChainPrecedence", true, new Template("chainPrecedence", false, false, true, false, false, false, true, false)),
-	CO_EXISTENCE("CoExistence", true, new Template("coExistence", true, true, true, true, false, false, false, false)),
-	SUCCESSION("Succession", true, new Template("succession", false, true, true, false, false, false, false, false)),
-	ALTERNATE_SUCCESSION("AlternateSuccession", true, new Template("alternateSuccession", false, true, true, false, true, true, false, false)),
-	CHAIN_SUCCESSION("ChainSuccession", true, new Template("chainSuccession", false, true, true, false, false, false, true, false)),
-	NOT_CHAIN_SUCCESSION("NotChainSuccession", false, new Template("notChainSuccession", false, true, true, false, false, false, true, true)),
-	NOT_SUCCESSION("NotSuccession", false, new Template("notSuccession", false, true, true, false, false, false, false, true)),
-	NOT_CO_EXISTENCE("NotCoExistence", false, new Template("notCoExistence", true, true, true, true, false, false, false, true));
+	RESPONDED_EXISTENCE("RespondedExistence",true, new Template("RespondedExistence", true, true, false, false, false, false, false, false) ),
+	RESPONSE("Response", true, new Template("Response", false, true, false, false, false, false, false, false)),
+	ALTERNATE_RESPONSE("AlternateResponse", true, new Template("AlternateResponse", false, true, false, false, true, false, false, false)),
+	CHAIN_RESPONSE("ChainResponse", true, new Template("ChainResponse", false, true, false, false, false, false, true, false)),
+	PRECEDENCE("Precedence", true, new Template("Precedence", false, false, true, false, false, false, false, false)),
+	ALTERNATE_PRECEDENCE("AlternatePrecedence", true, new Template("AlternatePrecedence", false, false, true, false, false, true, false, false)),
+	CHAIN_PRECEDENCE("ChainPrecedence", true, new Template("ChainPrecedence", false, false, true, false, false, false, true, false)),
+	CO_EXISTENCE("CoExistence", true, new Template("CoExistence", true, true, true, true, false, false, false, false)),
+	SUCCESSION("Succession", true, new Template("Succession", false, true, true, false, false, false, false, false)),
+	ALTERNATE_SUCCESSION("AlternateSuccession", true, new Template("AlternateSuccession", false, true, true, false, true, true, false, false)),
+	CHAIN_SUCCESSION("ChainSuccession", true, new Template("ChainSuccession", false, true, true, false, false, false, true, false)),
+	NOT_CHAIN_SUCCESSION("NotChainSuccession", false, new Template("NotChainSuccession", false, true, true, false, false, false, true, true)),
+	NOT_SUCCESSION("NotSuccession", false, new Template("NotSuccession", false, true, true, false, false, false, false, true)),
+	NOT_CO_EXISTENCE("NotCoExistence", false, new Template("NotCoExistence", true, true, true, true, false, false, false, true));
 	
 	private String templateLabel;
 	private Template template;
